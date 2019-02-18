5aefe88153a5aaa6c3968526c3f2bf1b0e49f699
Mateus Koppe <mateus.koppe@gmail.com>
2019-02-18 01:43:47 -0300

    Add project cards

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,24 @@
   <section class="container">
     <div>
       <h1 class="title">
-        Seja bem vindo
+        Mateus Koppe
       </h1>
-      <h2 class="subtitle">
-        Mateus Koppe Developer
+      <p class="quote">Olá, sou desenvolvedor web. Desenvolvo sites institucionais e sistemas de baixa e média complexidade, utilizo tecnologias atuais e trabalho priozando a perfomance e as boas práticas.</p>
+      <br>
+      <hr>
+      <br>
+      <h2 class="title">
+        Projetos
       </h2>
-      {{projects}}
+      <div>
+        <div
+          class="project-card"
+          v-for="(project, index) in projects"
+          :key="index"
+          >
+          {{project.name}}
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -36,17 +48,35 @@ export default {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+h1.title {
+  font-size: 100px;
+}
+
+h2.title {
+  font-size: 70px;
+}
+
+.quote {
+  color: #35495e;
+  max-width: 700px;
+  text-align: left;
+  padding-left: 20px;
+  border-left: #35495e solid 3px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+
+.project-card {
+  display: inline-block;
+  box-shadow: 0 .3px 5px .2px rgba(30, 30, 30, .3);
+  max-width: 300px;
+  color: #35495e;
+  padding: 15px;
+  margin: 10px;
 }
 
 .links {
