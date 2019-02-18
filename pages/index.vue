dd145bfc2f5788e409536ef237dd0c0b8bfbb0c8
Mateus Koppe <mateus.koppe@gmail.com>
2019-02-17 22:37:13 -0300

    Remove some initial code

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,18 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        portfolio
+        Seja bem vindo
       </h1>
       <h2 class="subtitle">
-        Mateus Koppe's Portifolio
+        Mateus Koppe Developer
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
 }
 </script>
 
