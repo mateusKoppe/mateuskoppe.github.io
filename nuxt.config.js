b7ceadad4c3699467da9bbc68723e662c4421bce
Mateus Koppe <mateus.koppe@gmail.com>
2019-03-04 15:22:30 -0300

    New logo and color theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,12 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'portfolio',
+    title: 'Mateus Koppe | Web Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Mateus Koppe&apos;s Portifolio' }
+      { name: 'theme-color', content: '#6843FF' },
+      { hid: 'description', name: 'description', content: 'Mateus Koppe - Web Developer Freelancer - Portfolio' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -16,7 +17,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#6843FF' },
   /*
   ** Plugins
   */
